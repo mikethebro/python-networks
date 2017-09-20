41652236a817db03b808d533747a986ed831eb49
mikethebro <michael.ward@aggiemail.usu.edu>
2017-09-20 11:45:48 -0600

    sends and receives

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 # server.py
 import socket
 import time
+import atexit
 
 # create a socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.0.118'
 TCP_PORT = 3000
 BUFFER_SIZE = 2048
 
@@ -13,22 +14,34 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-while True:
-    cmd = raw_input()
+# establish a connection
+print 'looking for connection...'
+conn, addr = s.accept()
+print 'Connection address:', addr
 
+while True:
+    print "type a command (listen or send):"
+    cmd = raw_input()
+    
     if (cmd == "listen"):
-        # establish a connection
-        conn, addr = s.accept()
-        print 'Connection address:', addr
-        while 1:
+        print "listening..."
+        t_end = time.time() + 15
+        while time.time() < t_end:
             data = conn.recv(BUFFER_SIZE)
             if not data: break
             print "received data:", data
-        conn.close()
+        #conn.close()
     elif (cmd == "send"):
         print "data to send: ",
         data = raw_input()
-        s.sendall(data)
+        conn.sendall(data)
+    elif (cmd == "exit"):
+        conn.close()
+
+def closing():
+    conn.close()
+
+atexit.register(closing)
 
 """
 # get local machine name
